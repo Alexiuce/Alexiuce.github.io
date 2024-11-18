5d054ee680ef6a5551c968d50ca74611786cfe43
alexiue <alexiuce@qq.com>
2024-11-18 20:53:29 +0800

    updaet header componet

---
+++ gitPage/src/components/HomeInfo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <div>
+
+
+
+
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
--- gitPage/src/views/HomeView.vue
+++ gitPage/src/views/HomeView.vue
@@ -1,7 +1,58 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+
+import HomeInfo from "@/components/HomeInfo.vue";
+
+const Titles = ['Home','Blog','History','About']
+
+
+
 </script>
 
 <template>
-  <TheWelcome />
+
+  <div class="container-view" flex flex-col w-full h-full>
+    <img src="@/assets/images/welcom-top.jpg" fixed  w-full h-full z--1 object-cover  alt="background"/>
+    <!--    header -->
+    <div h-80px w-full class="glass" justify-between>
+      <div>
+        <v-btn ml-40px  class="text-none" variant="text" v-for="(item,index) in Titles" :key="index">{{ item }}</v-btn>
+      </div>
+
+      <div flex >
+        <v-btn  ml-40px h-full class="text-none" variant="text"><v-icon icon="mdi-web"></v-icon>Language</v-btn>
+        <v-divider vertical color="success" :thickness="2"  class="border-opacity-25"></v-divider>
+        <v-btn   h-full class="text-none" variant="text">Theme</v-btn>
+      </div>
+
+    </div>
+
+
+    <div flex-1>
+
+      <component :is="HomeInfo" />
+
+    </div>
+
+
+
+  </div>
+
+
 </template>
+
+
+<style scoped lang="scss">
+
+.glass {
+  background: rgba(255, 255, 255, 0.05); /* 半透明背景 */
+  backdrop-filter: blur(10px); /* 毛玻璃模糊效果 */
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 阴影 */
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+</style>

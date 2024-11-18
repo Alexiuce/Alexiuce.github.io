49e5ba5df7376931c124e858aed66ab185aed655
alexiue <alexiuce@qq.com>
2024-11-18 13:37:38 +0800

    update page header

--- gitPage/src/components/TheWelcome.vue
+++ gitPage/src/components/TheWelcome.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
 
+const Titles = ['Home','Archives','History','Welcome']
+
+
+const dateString = new Date().toLocaleDateString('zh-CN');
+
+
 </script>
 
 <template>
 
   <div class="container-view" w-full h-full>
+    <img src="@/assets/images/welcom-top.jpg" fixed  w-full h-full z--1 object-cover  alt="background"/>
+
+<!--    header -->
+    <div h-80px w-full class="glass" justify-between>
+      <div>
+        <v-btn ml-40px h-full variant="text" v-for="(item,index) in Titles" :key="index">{{ item }}</v-btn>
+      </div>
+
+      <div flex >
+        <v-btn  ml-40px h-full variant="text">Language </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn   h-full variant="text">Theme </v-btn>
+      </div>
+
+
+
+    </div>
 
   </div>
 
@@ -13,12 +36,15 @@
 
 <style scoped lang="scss">
 
-.container-view{
-
-  background-image: url("@/assets/images/welcom-top.jpg");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+.glass {
+  background: rgba(255, 255, 255, 0.05); /* 半透明背景 */
+  backdrop-filter: blur(10px); /* 毛玻璃模糊效果 */
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 阴影 */
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-size: 1.5rem;
+  text-align: center;
 }
 
 
--- gitPage/src/main.ts
+++ gitPage/src/main.ts
@@ -2,12 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 import App from './App.vue'
 import router from './router'
 import 'virtual:uno.css'
 const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
+app.use(vuetify);
 app.use(createPinia())
 app.use(router)
 
